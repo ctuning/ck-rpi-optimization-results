6fad530c9cb2c0746e20ec889994b75c3ef910f8
Grigori Fursin <grigori.fursin@cTuning.org>
2017-07-28 14:38:24 +0200

    added more scripts (crowdtuning, complexity reduction)

---
+++ script/rpi3-crowdtune/crowdsource-autotuning.sh
@@ -0,0 +1,3 @@
+# Grigori Fursin prepared these low-level scripts for RPi3 autotuning and crowd-tuning
+
+ck crowdsource program.optimization --iterations=100 --repetitions=2 --scenario=8289e0cf24346aa7

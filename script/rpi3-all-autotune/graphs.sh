804d8bb749dbbd3391fb3a63f1618bc786d14d60
Grigori Fursin <grigori.fursin@cTuning.org>
2017-08-14 12:04:16 +0200

    updating slides with URLs

---
+++ script/rpi3-all-autotune/graphs.sh
@@ -0,0 +1,53 @@
+# ck plot graph:rpi3-7z-encode-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-7z-encode-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-7z-encode-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-7z-encode-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-ccrypt-encrypt-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-ccrypt-encrypt-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-ccrypt-encrypt-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-ccrypt-encrypt-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-gzip-decode-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-gzip-decode-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-gzip-decode-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-gzip-decode-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-gzip-encode-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-gzip-encode-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-gzip-encode-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-gzip-encode-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-minigzip-decode-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-minigzip-decode-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-minigzip-decode-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-minigzip-decode-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-minigzip-encode-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-minigzip-encode-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-minigzip-encode-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-minigzip-encode-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-rhash-sha3-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-rhash-sha3-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-rhash-sha3-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-rhash-sha3-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-sha512sum-sha512-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-sha512sum-sha512-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-sha512sum-sha512-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-sha512sum-sha512-gcc7-cbest-frontier
+
+# ck plot graph:rpi3-unrar-unrar-gcc4-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-unrar-unrar-gcc4-cbest-frontier
+
+# ck plot graph:rpi3-unrar-unrar-gcc7-cbest-frontier @graphs.json
+# ck browser experiment:rpi3-unrar-unrar-gcc7-cbest-frontier
